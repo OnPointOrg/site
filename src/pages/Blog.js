7ff3b082fa4185926bf768ac8796beaecc76410c
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-10 13:45:45 -0700

    changing from grid to flex boxes. should be fun

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ThemeProvider, theme, Text, Grid, Box, Divider } from '@chakra-ui/core';
+import { ThemeProvider, theme, Text, Grid, Box, Divider, Flex } from '@chakra-ui/core';
 import DefaultNav from '../components/DefaultNav';
 import VerifiedNav from '../components/VerifiedNav';
 import BlogPost from '../components/BlogPost';
@@ -9,16 +9,21 @@ export class Blog extends Component {
         return (
             <ThemeProvider theme={theme}>
                 <DefaultNav />
-                <Grid templateColumns="repeat(5, 2fr)" gap={3}>
-                    <Text fontSize="2xl">Trending Blogs</Text>
-                </Grid>
-                <Divider mt={5} mb={5} />
-                <Grid templateColumns="repeat(5, 2fr)" gap={3}>
-                    <BlogPost />
-                    <BlogPost />
-                    <BlogPost />
-                    <BlogPost />
-                </Grid>
+                <Flex align="center">
+                    <Flex bg="green.50" align="flex-end">
+                        <Text>Box 1</Text>
+                    </Flex>
+                    <Flex bg="blue.50" width="50px" align="center" justify="center">
+                        <Text textAlign="center" bg="pink.50">
+                            Box 2
+                        </Text>
+                    </Flex>
+                    <Box>
+                        <Text bg="tomato" color="white">
+                            Box 3
+                        </Text>
+                    </Box>
+                </Flex>
             </ThemeProvider>
         )
     }
