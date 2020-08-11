08c0afeeb6a45e5932b7b6b5c6ddf2b71ee5adbf
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-11 14:36:33 -0700

    added some buttons for the landing page

--- src/pages/MainHome.js
+++ src/pages/MainHome.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import DefaultNav from '../components/DefaultNav';
 import VerifiedNav from '../components/VerifiedNav';
-import { ThemeProvider, theme, Heading, Link as ChakraLink, Text, Flex, Image, Grid, Box, Button } from '@chakra-ui/core';
+import { ThemeProvider, theme, Heading, Link as ChakraLink, Text, Flex, Image, Grid, Box, Button, SimpleGrid } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
 import img from "../images/logo.png";
 
@@ -9,17 +9,16 @@ const MainHome = () => {
     return (
         <ThemeProvider theme={theme}>
             <DefaultNav />
-            {/* <Heading textAlign="center" fontSize="100px">👋 Hey</Heading> */}
-            {/* <br /> */}
-            <Heading textAlign="center" fontSize="65px">We Are OnPoint</Heading>
-            <Text marginTop="5px" textAlign="center"><Button><ChakraLink color="teal.500"><Link to="/signup">Sign Up</Link></ChakraLink></Button> or <Button><ChakraLink color="teal.500"><Link to="/signin">Sign In</Link></ChakraLink></Button></Text>
-            <Grid templateColumns="repeat(3, 1fr)" marginTop="20px">
-                <Box w="100%" />
-                <Box w="95%" overflow="hidden">
-                    <Image size="100%" justifyContent="center" w="100%" src={img} rounded="small" />
-                </Box>
-                <Box w="100%" />
-            </Grid>
+            <Heading textAlign="center" marginTop="50px" fontSize="65px">We Are OnPoint</Heading>
+            <br />
+            <Text marginTop="10px" textAlign="center" fontSize="35px">We Want To Change The Way You See The Media Forever</Text>
+            <br />
+            <br />
+            <SimpleGrid columns={3} spacing={5}>
+                <Button size="lg" margin="10px" width="50%" marginLeft="250px">Learn More</Button>
+                <Button size="lg" margin="10px" width="50%" marginLeft="135px">Get Started</Button>
+                <Button size="lg" margin="10px" marginRight="250px">Start Reading</Button>
+            </SimpleGrid>
         </ThemeProvider>
     )
 }
