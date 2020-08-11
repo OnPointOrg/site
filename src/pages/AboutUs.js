f1037defe30abf38fc833473c1971dfdc92b5e10
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-11 16:01:33 -0700

    added image to about page and centered it with simple grid

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import DefaultNav from '../components/DefaultNav';
 import VerifiedNav from '../components/VerifiedNav';
-import { ThemeProvider, theme, Heading, Link as ChakraLink, Text, Flex, Image, Grid, Box, Button } from '@chakra-ui/core';
+import { ThemeProvider, theme, Heading, Link as ChakraLink, Text, Flex, Image, Grid, Box, Button, SimpleGrid } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
-import img from "../images/logo.png";
+import teamimg from "../images/team.jpg";
 
 const AboutUs = () => {
     return (
@@ -12,6 +12,14 @@ const AboutUs = () => {
             <Heading textAlign="center" fontSize="100px">👋 Welcome</Heading>
             <br />
             <Heading textAlign="center" fontSize="65px">We Are OnPoint</Heading>
+            <SimpleGrid columns={4}>
+                <Box width="100%"></Box>
+                <Box width="200%">
+                    <Image src={teamimg} width="100%" marginTop="50px"></Image>
+                </Box>
+                <Box width="10%"></Box>
+                <Box width="100%"></Box>
+            </SimpleGrid>
         </ThemeProvider>
     )
 }
