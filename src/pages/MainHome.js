be280632a15e95ed2e5b74d750c12d5a60fd4d34
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-11 11:42:11 -0700

    starting the home page. looks ugly right now, will change after lunch

--- src/pages/MainHome.js
+++ src/pages/MainHome.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import DefaultNav from '../components/DefaultNav';
 import VerifiedNav from '../components/VerifiedNav';
-import { ThemeProvider, theme } from '@chakra-ui/core';
+import { ThemeProvider, theme, Heading, Link as chakraLink, Text } from '@chakra-ui/core';
+import { Link } from 'react-router-dom';
 
 const MainHome = () => {
     return (
         <ThemeProvider theme={ theme }>
             <DefaultNav />
-            <h1>This Is The Home Page</h1>
+            <Heading>👋 Hello</Heading>
+            <Heading>We Are OnPoint</Heading>
+            <Text>Get Started By <chakraLink><Link to="/signup">Signing Up</Link></chakraLink> or <chakraLink><Link to="/signin">Signing In</Link></chakraLink></Text>
         </ThemeProvider>
     )
 }
