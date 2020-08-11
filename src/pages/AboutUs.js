52816b33f99d56c4b1d984ccf28111169047aa97
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-11 13:52:11 -0700

    made part of the main home page and started the about

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react'
 import DefaultNav from '../components/DefaultNav';
-import { ThemeProvider, theme } from '@chakra-ui/core';
 import VerifiedNav from '../components/VerifiedNav';
+import { ThemeProvider, theme, Heading, Link as ChakraLink, Text, Flex, Image, Grid, Box, Button } from '@chakra-ui/core';
+import { Link } from 'react-router-dom';
+import img from "../images/logo.png";
 
-export class AboutUs extends Component {
-    render() {
-        return (
-            <div>
-                <ThemeProvider theme={theme}>
-                    <DefaultNav />
-                    <h1>About Us Page</h1>
-                </ThemeProvider>
-            </div>
-        )
-    }
+const AboutUs = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <DefaultNav />
+            <Heading textAlign="center" fontSize="100px">👋 Welcome</Heading>
+            <br />
+            <Heading textAlign="center" fontSize="65px">We Are OnPoint</Heading>
+        </ThemeProvider>
+    )
 }
 
-export default AboutUs
+export default AboutUs;
--- src/pages/MainHome.js
+++ src/pages/MainHome.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import DefaultNav from '../components/DefaultNav';
 import VerifiedNav from '../components/VerifiedNav';
-import { ThemeProvider, theme, Heading, Link as chakraLink, Text } from '@chakra-ui/core';
+import { ThemeProvider, theme, Heading, Link as ChakraLink, Text, Flex, Image, Grid, Box, Button } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
+import img from "../images/logo.png";
 
 const MainHome = () => {
     return (
-        <ThemeProvider theme={ theme }>
+        <ThemeProvider theme={theme}>
             <DefaultNav />
-            <Heading>👋 Hello</Heading>
-            <Heading>We Are OnPoint</Heading>
-            <Text>Get Started By <chakraLink><Link to="/signup">Signing Up</Link></chakraLink> or <chakraLink><Link to="/signin">Signing In</Link></chakraLink></Text>
+            {/* <Heading textAlign="center" fontSize="100px">👋 Hey</Heading> */}
+            {/* <br /> */}
+            <Heading textAlign="center" fontSize="65px">We Are OnPoint</Heading>
+            <Text marginTop="5px" textAlign="center"><Button><ChakraLink color="teal.500"><Link to="/signup">Sign Up</Link></ChakraLink></Button> or <Button><ChakraLink color="teal.500"><Link to="/signin">Sign In</Link></ChakraLink></Button></Text>
+            <Grid templateColumns="repeat(3, 1fr)" marginTop="20px">
+                <Box w="100%" />
+                <Box w="95%" overflow="hidden">
+                    <Image size="100%" justifyContent="center" w="100%" src={img} rounded="small" />
+                </Box>
+                <Box w="100%" />
+            </Grid>
         </ThemeProvider>
     )
 }
