72e673a0c22bdbeeec76d8c33f860069375e9db0
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-12 13:50:13 -0700

    finsihed the ui for the new story page. am going to work a little more on the signup stuff, because i can fix it since it began to work for sign in

--- src/pages/CreateStory.js
+++ src/pages/CreateStory.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import {
-  ThemeProvider,
-  theme,
-  Box,
-  Flex,
-  Button,
-  Checkbox,
-  Link as ChakraLink,
-  Text,
-  Stack,
-  Icon,
-  Input,
-  InputLeftElement,
-  FormControl,
-  FormLabel,
-  Heading,
-  Link,
-  InputGroup,
-  SimpleGrid,
+    ThemeProvider,
+    theme,
+    Box,
+    Flex,
+    Button,
+    Checkbox,
+    Link as ChakraLink,
+    Text,
+    Stack,
+    Icon,
+    Input,
+    InputLeftElement,
+    FormControl,
+    FormLabel,
+    Heading,
+    Link,
+    InputGroup,
+    SimpleGrid,
 } from "@chakra-ui/core";
 import DefaultNav from "../components/DefaultNav";
 import { Editor } from "@tinymce/tinymce-react";
@@ -25,79 +25,81 @@ import { Editor } from "@tinymce/tinymce-react";
 const VARIANT_COLOR = "teal";
 
 export class CreateStory extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <DefaultNav />
-        <Flex height="50%" width="100%" align="center" justifyContent="center">
-          <Box>
-            <Box p={4}>
-              <Box textAlign="center">
-                <Text>New Article</Text>
-                <Heading>Share The Story</Heading>
-              </Box>
-              <Box my={8} textAlign="left">
-                <form onSubmit={this.handleSubmit}>
-                  <SimpleGrid columns={3} spacing={10}>
-                    <Box width="100%" />
-                    <FormControl isRequired margin="25px" textAlign="center">
-                      <FormLabel>Title</FormLabel>
-                      <InputGroup>
-                        <InputLeftElement children={<Icon name="edit" />} />
-                        <Input
-                          // onChange={this.handleChange}
-                          width="100%"
-                          id="title"
-                          type="text"
-                          placeholder="A Very Interesting Title"
-                          // value={this.state.email}
-                        />
-                      </InputGroup>
-                    </FormControl>
-                    <Box width="100%" />
-                  </SimpleGrid>
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <DefaultNav />
+                <Flex height="50%" width="100%" align="center" justifyContent="center">
+                    <Box>
+                        <Box p={4}>
+                            <Box textAlign="center">
+                                <Text>New Article</Text>
+                                <Heading>Share The Story</Heading>
+                            </Box>
+                            <Box mt={1} textAlign="left">
+                                <form onSubmit={this.handleSubmit}>
+                                    <SimpleGrid columns={3} spacing={10}>
+                                        <Box width="100%" />
+                                        <FormControl isRequired margin="25px" textAlign="center">
+                                            <FormLabel>Title</FormLabel>
+                                            <InputGroup>
+                                                <InputLeftElement children={<Icon name="edit" />} />
+                                                <Input
+                                                    // onChange={this.handleChange}
+                                                    width="100%"
+                                                    id="title"
+                                                    type="text"
+                                                    placeholder="A Very Interesting Title"
+                                                // value={this.state.email}
+                                                />
+                                            </InputGroup>
+                                        </FormControl>
+                                        <Box width="100%" />
+                                    </SimpleGrid>
 
-                  <Editor
-                    apiKey="zybn8i03lzs6uwwunfnmni1kwzgvhne31xy1rwof0u56mdx6"
-                    initialValue="<p>This is the initial content of the editor</p>"
-                    init={{
-                      height: 600,
-                      width: 1000,
-                      menubar: true,
-                      plugins: [
-                        "emoticons",
-                        "advlist autolink lists link image charmap print preview anchor",
-                        "searchreplace visualblocks code fullscreen",
-                        "insertdatetime media table paste code help wordcount",
-                      ],
-                      toolbar:
-                        "undo redo | formatselect | bold italic backcolor emoticons | \
+                                    <Editor
+                                        apiKey="zybn8i03lzs6uwwunfnmni1kwzgvhne31xy1rwof0u56mdx6"
+                                        initialValue="<h1>Heading</h1> <p>Paragraph</p>"
+                                        init={{
+                                            height: 600,
+                                            width: 1000,
+                                            menubar: true,
+                                            plugins: [
+                                                "emoticons",
+                                                "advlist autolink lists link image charmap print preview anchor",
+                                                "searchreplace visualblocks code fullscreen",
+                                                "insertdatetime media table paste code help wordcount",
+                                            ],
+                                            toolbar:
+                                                "undo redo | formatselect | bold italic backcolor emoticons | \
                                                  alignleft aligncenter alignright alignjustify | \
                                                  bullist numlist outdent indent | removeformat | help",
-                    }}
-                    onEditorChange={this.handleEditorChange}
-                  />
-                  <SimpleGrid columns={3} spacing={10}>
-                    <Box width="100%" />
-                    <Box width="100%" />
-                    
-                        <Button
-                          type="submit"
-                          variantColor={VARIANT_COLOR}
-                          width="full"
-                          mt={4}
-                        >
-                          Submit
-                        </Button>
-                  </SimpleGrid>
-                </form>
-              </Box>
-            </Box>
-          </Box>
-        </Flex>
-      </ThemeProvider>
-    );
-  }
+                                        }}
+                                        onEditorChange={this.handleEditorChange}
+                                    />
+                                    <SimpleGrid columns={3} spacing={10}>
+                                        <Box width="100%" />
+                                        <Box width="100%" />
+                                        <Button
+                                            leftIcon="check"
+                                            size="lg"
+                                            type="submit"
+                                            variantColor={VARIANT_COLOR}
+                                            variant="solid"
+                                            width="full"
+                                            mt={4}
+                                        >
+                                            Submit
+                                        </Button>
+                                    </SimpleGrid>
+                                </form>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Flex>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default CreateStory;
