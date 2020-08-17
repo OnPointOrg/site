ce84c301773d456ea282f92ce79943f6c68e1629
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-17 15:21:29 -0700

    started switch statements to the function

--- src/hooks/ReadArticlesFromFirebase.js
+++ src/hooks/ReadArticlesFromFirebase.js
@@ -1,5 +1,7 @@
+import React, { Component } from 'react';
 import firebase from 'firebase'
 import firestoreDatabase from '../firebase/config';
+import { Heading } from '@chakra-ui/core';
 
 const getDocs = () => {
     firestoreDatabase.collection("articles").get().then((querySnapshot) => {
@@ -11,14 +13,23 @@ const getDocs = () => {
             console.log(article.title);
             console.log(article.category);
             console.log(article.user);
+            console.log(article.content.blocks[0].type)
             console.log(article.content.blocks[0].data.text)
             const contentType = article.content.blocks[0].type
             const content = article.content.blocks[0].data.text
-            if (contentType === 'paragraph') {
-                createElement('Text', content);
-                console.log(createElement('Text', content))
-            } else {
-                console.log('Unable To Create Element!')
+            // if (contentType === 'paragraph') {
+            //     createElement('Text', content);
+            //     console.log(createElement('Text', content))
+            // } else {
+            //     console.log('Unable To Create Element!')
+            // }
+
+            switch(content) {
+                case "paragraph":
+                    createElement(contentType, content)
+                    break;
+                case "header":
+                    createElement(contentType, content)
             }
         });
     });
