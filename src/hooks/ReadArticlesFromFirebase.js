802682a353385d848164ac134923ac0465475b0d
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-18 15:26:26 -0700

    added a way to make list make an actual usable List element

--- src/hooks/ReadArticlesFromFirebase.js
+++ src/hooks/ReadArticlesFromFirebase.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import firebase from 'firebase'
 import firestoreDatabase from '../firebase/config';
-import { Heading } from '@chakra-ui/core';
+import { Heading, List } from '@chakra-ui/core';
 
 const getDocs = () => {
     firestoreDatabase.collection("articles").get().then((querySnapshot) => {
@@ -10,28 +10,41 @@ const getDocs = () => {
         querySnapshot.forEach((doc) => {
             console.log(doc.id + ' ========== ' + doc.data());
             const article = doc.data();
-            // console.log(article.title);
-            // console.log(article.category);
-            // console.log(article.user);
-            // console.log(article.content.blocks[0].type)
-            // console.log(article.content.blocks[0].data.text)
+            console.log(article.title);
+            console.log(article.category);
+            console.log(article.user);
+            console.log(article.content.blocks[0].type)
+            console.log(article.content.blocks[0].data.text)
 
             let contentBlockLength = article.content.blocks.length;
             for (let i = 0; i < contentBlockLength; i++) {
                 const contentType = article.content.blocks[i].type
-                const content = article.content.blocks[i].data.text
+
 
                 switch (contentType) {  
                     case "paragraph":
+                        const paragraphText = article.content.blocks[i].data.text
                         console.log("-----Element-----")
-                        console.log(createElement('Text', content))
+                        console.log(createElement('Text', paragraphText))
                     case "header":
-                        createElement('Heading', content)
-                        break;
+                        const headerText = article.content.blocks[i].data.text
+                        createElement('Heading', headerText)
                     case "list":
-                        createElement('List', content)
+                        const items = article.content.blocks[i].data.items
+                        const allItems = [];
+                        for(let j = 0; j < items.length; j++) {
+                            allItems.push(createElement('ListItem', items[j]));
+                            console.log(createElement('ListItem', items[j]))
+                        }
+
+                        const listArray = []
+
+                        createElement('List', allItems.join(""))
                         console.log("-----Element-----")
-                        console.log(createElement('List', content))
+                        console.log(createElement('List', allItems.join("")))
+
+                    case "warning":
+
                 }
             }
         });
