f159adcc7709d67a08fe89c3e3bc43ce446f932c
Aditya Rawat <aditya1rawat@gmail.com>
2020-08-21 17:48:46 -0700

    Update ReadArticlesFromFirebase.js

--- src/hooks/ReadArticlesFromFirebase.js
+++ src/hooks/ReadArticlesFromFirebase.js
@@ -116,25 +116,7 @@ const createSelfCloseTag = (type, attributes = "") => {
 
 const createQuoteElement = (quote, credits) => {
     return `
-          <Box width="900px" borderWidth="1px" rounded="lg" overflow="hidden" margin="50px">
-          <Box marginTop="50px" marginLeft="50px">
-          <Heading>
-          <FaQuoteLeft />
-          </Heading>
-          </Box>
-          <Box>
-          <Box marginTop="30px" fontWeight="semibold" as="h4" lineHeight="tight">
-          <Text fontSize="xl" mx="100px">
-          ${quote}
-          </Text>
-          <Box d="flex" alignItems="center" color="gray.600" marginTop="50px" margin="50px">
-          <Text fontSize="lg">
-          - ${credits}
-          </Text>
-          </Box>
-          </Box>
-          </Box>
-          </Box>
+          <Box width="900px" borderWidth="1px" rounded="lg" overflow="hidden" margin="50px"><Box marginTop="50px" marginLeft="50px"><Heading><FaQuoteLeft /></Heading></Box><Box><Box marginTop="30px" fontWeight="semibold" as="h4" lineHeight="tight"><Text fontSize="xl" mx="100px">${quote}</Text><Box d="flex" alignItems="center" color="gray.600" marginTop="50px" margin="50px"><Text fontSize="lg">- ${credits}</Text></Box></Box></Box></Box>
           `;
 };
 
