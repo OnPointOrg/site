49773647825ced45a0259b1bb0268dc8dc9d610f
Aditya Rawat <aditya1rawat@gmail.com>
2020-10-23 09:21:41 -0700

    Revert "the env files are broken right now, working on fixing it"
    This reverts commit 9ed16eccb7a592d63a9600fa859b3f2d90732e9a.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,26 +1,14 @@
 import * as firebase from "firebase";
 
-const dotenv = require('dotenv');
-dotenv.config({ path: '../../.env' }); 
-const apiKey = process.env.API_KEY;
-const authDomain = process.env.AUTH_DOMAIN;
-const databaseURL = process.env.DATABASE_URL;
-const projectId = process.env.PROJECT_ID;
-const storageBucket = process.env.STORAGE_BUCKET;
-const messagingSenderId = process.env.MESSAGING_SENDER_ID;
-const appId = process.env.APP_ID;
-const measurementId = process.env.MEASUREMENT_ID;
-
-
 const config = {
-  apiKey: apiKey,
-  authDomain: authDomain,
-  databaseURL: databaseURL,
-  projectId: projectId,
-  storageBucket: storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appId,
-  measurementId: measurementId,
+  apiKey: "AIzaSyCikY7FxY0XE0hPeSwrpcAgmj61H8RshyY",
+  authDomain: "onpointnewsorg.firebaseapp.com",
+  databaseURL: "https://onpointnewsorg.firebaseio.com",
+  projectId: "onpointnewsorg",
+  storageBucket: "onpointnewsorg.appspot.com",
+  messagingSenderId: "65120918931",
+  appId: "1:65120918931:web:ae558371d2cc82efc21c66",
+  measurementId: "G-4MCLR7Y6CV",
 };
 
 firebase.initializeApp(config);
