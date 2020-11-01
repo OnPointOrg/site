1e88d52157ce1fba48bbd9728bf461793d5bf288
Aditya Rawat <aditya1rawat@gmail.com>
2020-10-31 23:43:10 -0700

    worked on about page a bit more, still need to add aditya girish, aditya hari, and aditya guntupalli

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { Box, Image, Tag } from "@chakra-ui/core";
+import { Box, Image, Tag, Text } from "@chakra-ui/core";
 
 export const About = (props) => {
   return (
     <div>
       <Box w="100%">
-        <Box maxW="sm" borderWidth="1px" rounded="lg">
+        <Box width="400px" maxHeight="800px" borderWidth="1px" rounded="lg">
           <Image
             src={props.img}
             alt={props.name}
             overflow="hidden"
             rounded="md"
-            mb="-100px"
+            width="100%"
+            height="auto"
           />
-          <Box px="6" fontWeight="bold" as="h4" lineHeight="tight" mb="-15px">
-            {props.name}
+          <Box px="6" fontWeight="bold" as="h4" lineHeight="tight" mt={"20px"}>
+            <Text fontSize="25px">{props.name}</Text>
           </Box>
 
           <Box p="6">
@@ -41,6 +42,54 @@ export const About = (props) => {
           </Box>
         </Box>
       </Box>
+
+      {/* Trying Something New Here
+      <Box w="100%" maxHeight="600px">
+        <Box width="400px" borderWidth="1px" rounded="lg">
+          <Grid templateColumns="repeat(2, 1fr)" gap={1}>
+            <Image
+              src={props.img}
+              alt={props.name}
+              overflow="hidden"
+              rounded="md"
+              width="80%"
+            />
+            <Box px="6" mt="70px" fontWeight="bold" as="h4" lineHeight="tight">
+              <Text as="h3" fontSize="25px" textAlign="center">{props.name}</Text>
+              <Box d="flex" alignItems="baseline">
+                {props.roles.map((role) => {
+                  return (
+                    <Box>
+                      <Tag
+                        size={"sm"}
+                        key={"sm"}
+                        variantColor="gray"
+                        mr={"5px"}
+                      >
+                        {role}
+                      </Tag>
+                    </Box>
+                  );
+                })}
+              </Box>
+            </Box>
+          </Grid>
+
+          <Box p="6">
+            <Box
+              pt="3"
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              textTransform="uppercase"
+              mt="5px"
+            >
+              {props.bio}
+            </Box>
+          </Box>
+        </Box>
+      </Box> */}
     </div>
   );
 };
