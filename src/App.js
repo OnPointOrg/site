921241a12de85893b34ad7f09015008e12c816aa
Aditya Rawat <aditya1rawat@gmail.com>
2020-10-31 23:14:51 -0700

    fixed a bunch of theming issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import {
-  ThemeProvider,
-  theme,
-  ColorModeProvider,
-  CSSReset,
-} from "@chakra-ui/core";
+// import { ThemeProvider, ColorModeProvider, CSSReset } from "@chakra-ui/core";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
@@ -16,27 +11,29 @@ import NewStory from "./pages/NewStory";
 import ForgotPassword from "./pages/ForgotPassword";
 import BlogContentPost from "./components/blog/BlogContentPost";
 
+// import customTheme from "./components/theme/theme.js";
+
 export class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <CSSReset />
-          <Router>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/signup" component={SignUp} />
-              <Route path="/signin" component={SignIn} />
-              <Route path="/about" component={AboutUs} />
-              <Route path="/contact" component={ContactUs} />
-              <Route path="/blog" exact component={Blog} />
-              <Route path="/newstory" component={NewStory} />
-              <Route path="/blog/:docId" component={BlogContentPost} />
-              <Route path="/forgotpassword" component={ForgotPassword} />
-            </Switch>
-          </Router>
-        </ColorModeProvider>
-      </ThemeProvider>
+      // <ThemeProvider theme={customTheme}>
+      //   <ColorModeProvider>
+      //     <CSSReset />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/about" component={AboutUs} />
+          <Route path="/contact" component={ContactUs} />
+          <Route path="/blog" exact component={Blog} />
+          <Route path="/newstory" component={NewStory} />
+          <Route path="/blog/:docId" component={BlogContentPost} />
+          <Route path="/forgotpassword" component={ForgotPassword} />
+        </Switch>
+      </Router>
+      //   </ColorModeProvider>
+      // </ThemeProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { ThemeProvider, ColorModeProvider, CSSReset } from "@chakra-ui/core";
+
+import customTheme from "./components/theme/theme.js";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={customTheme}>
+      <ColorModeProvider>
+        <CSSReset />
+        <App />
+      </ColorModeProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
