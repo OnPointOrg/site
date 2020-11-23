f2f3f1e55a0e7f60b2c7dee05381abd17651d376
Aditya Rawat <aditya1rawat@gmail.com>
2020-11-23 02:29:52 -0800

    working on transparency navbar fixes

--- src/components/nav/DefaultNav.js
+++ src/components/nav/DefaultNav.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import {
   Box,
   Flex,
-  Divider,
   Heading,
   Button,
   Image,
   Link as ChakraLink,
   Stack,
+  Text,
 } from "@chakra-ui/core";
 import ThemeButton from "./ThemeButton";
 import logo from "../../images/logo.png";
@@ -21,7 +21,19 @@ breakpoints.xl = breakpoints[3];
 
 const DefaultNav = (props) => {
   return (
-    <Box as="header" shadow="md" fontWeight="bold">
+    <Box
+      as="header"
+      shadow="md"
+      fontWeight="bold"
+      position="fixed"
+      transition="box-shadow 0.2s"
+      top="0"
+      opacity="100%"
+      width="100%"
+      zIndex="1"
+      left="0"
+      right="0"
+    >
       <Stack isInline justifyContent="space-between" alignItems="center" py={4}>
         <Box>
           <Flex align="center" ml={5}>
@@ -32,28 +44,22 @@ const DefaultNav = (props) => {
             </Link>
             <Box ml={10}>
               <Link to="/about">
-                <Button variantColor="teal" variant="ghost">
-                  <ChakraLink p={4} rounded="md">
-                    About
-                  </ChakraLink>
+                <Button variantColor="teal" variant="ghost" rounded="full">
+                  <ChakraLink p={4}>About</ChakraLink>
                 </Button>
               </Link>
             </Box>
             <Box>
               <Link to="/blog">
-                <Button variantColor="teal" variant="ghost">
-                  <ChakraLink p={4} rounded="md">
-                    Blog
-                  </ChakraLink>
+                <Button variantColor="teal" variant="ghost" rounded="full">
+                  <ChakraLink p={4}>Blog</ChakraLink>
                 </Button>
               </Link>
             </Box>
             <Box>
               <Link to="/contact">
-                <Button variantColor="teal" variant="ghost">
-                  <ChakraLink p={4} rounded="md">
-                    Contact
-                  </ChakraLink>
+                <Button variantColor="teal" variant="ghost" rounded="full">
+                  <ChakraLink p={4}>Contact</ChakraLink>
                 </Button>
               </Link>
             </Box>
@@ -63,13 +69,16 @@ const DefaultNav = (props) => {
           <Stack isInline spacing={4} alignItems="center">
             <Flex justifyContent="space-between" color="gray.500">
               <Link to="/signin">
-                <Button size="md" variant="solid">
+                <Button size="md" variant="solid" rounded="full">
                   Sign In
                 </Button>
               </Link>
-              <Divider orientation="vertical" />
+              {/* <Divider orientation="vertical" /> */}
+              <Text isInline mt="6px" mx="10px">
+                /
+              </Text>
               <Link to="/signup">
-                <Button size="md" variant="outline">
+                <Button size="md" variant="outline" rounded="full">
                   Sign Up
                 </Button>
               </Link>
@@ -80,7 +89,6 @@ const DefaultNav = (props) => {
           </Stack>
         </Box>
       </Stack>
-      <Divider />
     </Box>
   );
 };
