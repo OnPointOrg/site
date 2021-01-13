417fa785c6d1f59c34ce217861d2af065a8485ed
Aditya Rawat <aditya1rawat@gmail.com>
2021-01-13 10:34:24 -0800

    removed loading spinner for now

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ import ForgotPassword from './pages/ForgotPassword';
 import ArticleContentPost from './components/article/ArticleContentPost';
 
 export class App extends Component {
-   componentDidMount = () => {
-      console.log('Loaded');
-   };
-
    render() {
       return (
          <Router>
--- src/index.js
+++ src/index.js
@@ -5,22 +5,13 @@ import App from './App';
 import { ThemeProvider, ColorModeProvider, CSSReset } from '@chakra-ui/core';
 
 import customTheme from './components/theme/theme.js';
-import Loading from './components/home/Loading';
-
-const showLoader = () => {
-   return <Loading />;
-};
-
-const hideLoader = () => {
-   return <Loading style={{ display: 'none' }} />;
-};
 
 ReactDOM.render(
    <React.StrictMode>
       <ThemeProvider theme={customTheme}>
          <ColorModeProvider>
             <CSSReset />
-            <App showLoader={showLoader} hideLoader={hideLoader} />
+            <App />
          </ColorModeProvider>
       </ThemeProvider>
    </React.StrictMode>,
