58256884b879394fe42e684ee97b1d4ec301054d
Aditya Rawat <aditya1rawat@gmail.com>
2021-06-30 16:10:33 -0700

    don't use admin anymore so don't need this

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -13,19 +13,6 @@ const config = {
 
 console.log(process.env.REACT_APP_FIREBASE_ADMIN_KEY);
 
-// admin.initializeApp({
-//    credential: admin.credential.cert(
-//       JSON.parse(
-//          Buffer.from(
-//             process.env.REACT_APP_FIREBASE_ADMIN_KEY,
-//             'base64'
-//          ).toString()
-//       )
-//    ),
-//    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-//    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
-// });
-
 firebase.initializeApp(config);
 firebase.analytics();
 
