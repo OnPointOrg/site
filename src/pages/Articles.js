4995059cc9fb2c7432a0624ac8e40a7cde11539c
Aditya Rawat <aditya1rawat@gmail.com>
2021-10-23 04:24:03 +0000

    some more additions

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -11,27 +11,31 @@ class Articles extends Component {
         page: <Loading />
     };
 
-    componentDidMount = () => {
-        firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                this.setState({
-                    page: <ArticleGrid />
-                });
-            } else {
-                this.setState({
-                    page: (
-                        <GetStarted
-                            text="You cannot view our collection of articles without an account!
-               Make your free account to view all of our articles!"
-                        />
-                    )
-                });
-            }
-        });
-    };
+    // componentDidMount = () => {
+    //     firebase.auth().onAuthStateChanged(user => {
+    //         if (user) {
+    //             this.setState({
+    //                 page: <ArticleGrid />
+    //             });
+    //         } else {
+    //             this.setState({
+    //                 page: (
+    //                     <GetStarted
+    //                         text="You cannot view our collection of articles without an account!
+    //            Make your free account to view all of our articles!"
+    //                     />
+    //                 )
+    //             });
+    //         }
+    //     });
+    // };
 
     render() {
-        return <Box width="100%">{this.state.page}</Box>;
+        return (
+            <Box width="100%">
+                <ArticleGrid />
+            </Box>
+        );
     }
 }
 
